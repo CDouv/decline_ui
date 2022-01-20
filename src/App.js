fa7c01c00d4067ff32160ece210bdc5a082ff5d7
Chris <christopher.douvry@gmail.com>
2022-01-19 20:50:02 -0600

    Working on sending parameters over to Rust backend

--- src/App.js
+++ src/App.js
@@ -1,141 +1,152 @@
-import Header from './components/Header'
-import Parameters from './components/Parameters'
-import Calculate from './components/Calculate'
+import Header from "./components/Header";
+import Parameters from "./components/Parameters";
+import Calculate from "./components/Calculate";
 
-import './App.css';
-import { useState} from 'react'
-import { queries } from '@testing-library/react';
-const axios = require('axios').default;
-
+import "./App.css";
+import { useState } from "react";
+import { queries } from "@testing-library/react";
+const axios = require("axios").default;
 
 const App = () => {
+  const [parameters, setParameters] = useState([
+    {
+      text: "Initial Flow Rate",
+      symbol: "qi",
+      units: "bbl/d",
+      calculate: false,
+      input: null,
+    },
+
+    {
+      text: "Final Flow Rate",
+      symbol: "qf",
+      units: "bbl/d",
+      calculate: false,
+      input: null,
+    },
+
+    {
+      text: "Decline Rate",
+      symbol: "d",
+      units: "%/yr",
+      calculate: false,
+      input: null,
+    },
+
+    {
+      text: "Segment Duration",
+      symbol: "t",
+      units: "years",
+      calculate: false,
+      input: null,
+    },
+
+    {
+      text: "Segment Reserves",
+      symbol: "np",
+      units: "mbbl",
+      calculate: false,
+      input: null,
+    },
+  ]);
+
+  const exportParameters = () => {
+    // let inputs = [];
+    // parameters.map((parameter) => inputs.push(parameter));
+    // let jsonInputs = { ...inputs };
+    let jsonInputs = parameters[0];
+
+    return jsonInputs;
+  };
+
+  const countUnknowns = () => {
+    let knownsCount = 0;
+    let unknownsCount = 0;
+
+    parameters.map((parameter) =>
+      parameter.calculate === true ? (knownsCount += 1) : (unknownsCount += 1)
+    );
+
+    console.log(
+      `There are ${knownsCount} knowns and ${unknownsCount} unknowns`
+    );
+
+    return knownsCount, unknownsCount;
+  };
+
+  let data = exportParameters();
+  data = JSON.stringify(data);
+  console.log(data);
+  console.log(typeof data);
 
   // Test connecting to back-end
-  const getData = async () => {
+
+  const sendJSON = async () => {
+    let url = "http://localhost:8000/solve";
+
+    var request = new XMLHttpRequest();
+    request.open("POST", url, true);
+    request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    request.send(data);
+    console.log(data);
     const response = await axios({
-      method: 'get',
-      url: "http://localhost:8000/api/myrocket",
+      method: "post",
+      url: "http://localhost:8000/solve",
       withCredentials: false,
     });
 
     console.log(response.data);
-
   };
 
-  // getData();
+  sendJSON();
 
-  const [parameters, setParameters] = useState([
-    {
-      text:'Initial Flow Rate',
-      symbol:'qi',
-      units:'bbl/d',
-      calculate: false,
-      input:null,
-      
-     
+  const toggleCalculate = (symbol) => {
+    const newParameters = parameters.map((parameter) => {
+      if (parameter.symbol === symbol) {
+        const newCalculate = !parameter.calculate;
+        if (newCalculate) {
+          return { ...parameter, calculate: newCalculate };
+        } else {
+          return { ...parameter, calculate: newCalculate, input: null };
+        }
+      } else {
+        return parameter;
+      }
+    });
+    setParameters(newParameters);
+  };
 
-    },
+  const toggleChangeInput = (symbol, val) => {
+    setParameters(
+      parameters.map((parameter) =>
+        parameter.symbol === symbol ? { ...parameter, input: val } : parameter
+      )
+    );
+  };
 
-    {
-      text:'Final Flow Rate',
-      symbol:'qf',
-      units:'bbl/d',
-      calculate: false,
-      input:null,
-      
-     
+  const clearInput = (symbol) => {
+    const newParameters = parameters.map((parameter) =>
+      parameter.symbol === symbol && !parameter.calculate
+        ? { ...parameter, input: null }
+        : parameter
+    );
+    console.log(newParameters);
 
-    },
-
-    {
-      text:'Decline Rate',
-      symbol:'d',
-      units:'%/yr',
-      calculate: false,
-      input:null,
-      
-     
-
-    },
-
-    {
-      text:'Segment Duration',
-      symbol:'t',
-      units:'years',
-      calculate: false,
-      input:null,
-      
-      
-
-    },
-
-    {
-      text:'Segment Reserves',
-      symbol:'np',
-      units:'mbbl',
-      calculate: false,
-      input:null,
-      
-
-    },
-  ])
-
-const exportParameters = () => {
-  let inputs = [];
-  parameters.map((parameter) => inputs.push(parameter.input));
-  let jsonInputs = {...inputs};
-
-  return jsonInputs
-}
-
-const countUnknowns = () => {
-  let knownsCount = 0;
-  let unknownsCount = 0;
-
-  parameters.map((parameter) =>parameter.calculate === true ?
-    knownsCount += 1 : unknownsCount += 1) 
-    
-  console.log(`There are ${knownsCount} knowns and ${unknownsCount} unknowns`);
-
-  return (knownsCount,unknownsCount)
-
-}
-
-
-console.log(exportParameters());
-
-const toggleCalculate = (symbol) => {
-  setParameters(parameters.map((parameter) => parameter.symbol === symbol 
- 
-  ? {...parameter, calculate: !parameter.calculate} : parameter
-
-  
-  ));
-
-}
-
-const toggleChangeInput = (symbol,val) => {
-  setParameters(parameters.map((parameter) => parameter.symbol === symbol
-  ? {...parameter, input:val} : parameter))
-}
-
-const clearInput = (symbol) => {
-  setParameters(parameters.map((parameter) => (parameter.symbol === symbol) || (!parameter.calculate)
-  ? {...parameter,input:null} : parameter))
-}
-
+    setParameters(newParameters);
+  };
 
   return (
-    <div className = "container">
-      <Header title = 'Decline Calculator'/>
-      <Parameters 
-        parameters = {parameters}
+    <div className="container">
+      <Header title="Decline Calculator" />
+      <Parameters
+        parameters={parameters}
         onToggle={toggleCalculate}
-        changeInput = {toggleChangeInput}
-        clearInput = {clearInput} />
-       <Calculate countUnknowns = {countUnknowns}/>
+        changeInput={toggleChangeInput}
+        clearInput={clearInput}
+      />
+      <Calculate countUnknowns={countUnknowns} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Parameter.js
+++ src/components/Parameter.js
@@ -1,40 +1,45 @@
-import { useState } from 'react'
-import Unknown from './inputs/Unknown'
-import Known from './inputs/Known'
+import { useState } from "react";
+import Unknown from "./inputs/Unknown";
+import Known from "./inputs/Known";
 
-export const Parameter = ({parameter, onToggle, changeInput, clearInput}) => {
-    
-
-    //function to render input
-    const renderInput = () => {
-        if (parameter.calculate) {
-            return <Known parameter = {parameter}
-                          changeInput = {changeInput} 
-                          onToggle = {onToggle} />
-         } else {
-            //  clearInput(parameter.symbol);
-             return <Unknown parameter = {parameter}
-                            changeInput = {changeInput}
-                            clearInput = {clearInput} />
-         }
+export const Parameter = ({ parameter, onToggle, changeInput, clearInput }) => {
+  //function to render input
+  const renderInput = () => {
+    if (parameter.calculate) {
+      return (
+        <Known
+          parameter={parameter}
+          changeInput={changeInput}
+          onToggle={onToggle}
+        />
+      );
+    } else {
+      //  clearInput(parameter.symbol);
+      return (
+        <Unknown
+          parameter={parameter}
+          changeInput={changeInput}
+          clearInput={clearInput}
+        />
+      );
     }
+  };
 
-    return (
-        <div className = 'parameter'>
+  return (
+    <div className="parameter">
+      <input
+        type="checkbox"
+        onClick={() => {
+          onToggle(parameter.symbol);
+          //    clearInput(parameter.symbol);
+        }}
+      />
 
-            <input type='checkbox'  onClick={() => {onToggle(parameter.symbol);
-                                                    // clearInput(parameter.symbol)
-                                                }}
-            />
+      <div className="symbol">{parameter.symbol}</div>
 
-            <div className = 'symbol'>
-            {parameter.symbol}
-            </div>
+      {renderInput()}
+    </div>
+  );
+};
 
-            {renderInput()}
-
-        </div>
-    )
-}
-
-export default Parameter
+export default Parameter;
